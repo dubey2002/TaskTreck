454c940cc77b0810ac8db085c0f019c02da7f1c5
Anurag Dubey <98604732+dubey2002@users.noreply.github.com>
2024-04-06 17:08:42 +0530

    Add files via upload

---
+++ myfirstwebapp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'anurag', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'anurag', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'anurag', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'anurag', 'Learn DevOps', CURRENT_DATE(), false);
